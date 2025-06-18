3c1a93d65edc6e0373624f61273e2217847965be
carlosportella16 <carlosportella16@hotmail.com>
2025-06-18 15:07:33 -0300

    feat: creating pixms project

---
+++ pixms/src/main/java/com/tech/pixms/PixmsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.pixms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixmsApplication.class, args);
+	}
+
+}
---
+++ pixms/src/test/java/com/tech/pixms/PixmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.pixms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
