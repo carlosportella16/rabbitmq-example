12295e6e9fbcbe480085ab90a37f5281a2bed760
carlosportella16 <carlosportella16@hotmail.com>
2025-06-18 15:49:09 -0300

    chore: folder name update pixms

--- pixms/src/main/java/com/tech/pixms/PixmsApplication.java
+++ pixms/src/main/java/com/tech/pixms/PixmsApplication.java
@@ -1,4 +1,4 @@
-package com.tech.pfms;
+package com.tech.pixms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pixms/src/test/java/com/tech/pixms/PixmsApplicationTests.java
+++ pixms/src/test/java/com/tech/pixms/PixmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tech.pfms;
+package com.tech.pixms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
