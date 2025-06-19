e4f73e1c30c1ab839c29ea1f7eb5abadd5f2650c
carlosportella16 <carlosportella16@hotmail.com>
2025-06-19 11:49:34 -0300

    feat: pix controller logs

--- pixms/src/main/java/com/tech/pixms/controller/PixController.java
+++ pixms/src/main/java/com/tech/pixms/controller/PixController.java
@@ -1,6 +1,8 @@
 package com.tech.pixms.controller;
 
 import com.tech.pixms.controller.dto.PixReq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import static com.tech.pixms.config.RabbitMqConfig.PIX_DONE_EXCHANGE;
 @RequestMapping("/pix")
 public class PixController {
 
+    private final Logger logger = LoggerFactory.getLogger(PixController.class);
+
     private final RabbitTemplate rabbitTemplate;
 
     public PixController(RabbitTemplate rabbitTemplate) {
@@ -23,6 +27,7 @@ public class PixController {
     @PostMapping
     public ResponseEntity<Void> makePix(@RequestBody PixReq pixReq) {
         rabbitTemplate.convertAndSend(PIX_DONE_EXCHANGE, pixReq.channel(), pixReq);
+        logger.info("PIX MS - PIX sent {}", pixReq);
         return ResponseEntity.accepted().build();
     }
 }
